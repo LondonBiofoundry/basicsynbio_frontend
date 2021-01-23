4e7ff07a3878e409e5132aa7b1157de8b74cc5e2
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-01-23 13:59:56 +0000

    removes API connection string from index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-export const ApiEndpoint = "https://flask-fire-xi5dtbaifq-nw.a.run.app/";
-//export const ApiEndpoint = "http://127.0.0.1:5000/";
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
