c575fa39240c4567bfab9fbf1f810d8d3a8dd7aa
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-03-09 10:29:38 +0000

    updates backend url

--- src/ApiConnection.ts
+++ src/ApiConnection.ts
@@ -1,5 +1,5 @@
 // To work with the hosted production backend, leave the first line uncommented
 // TO work with a local instance of the backend, uncomment the second line
 
-export const ApiEndpoint = "https://flask-fire-xi5dtbaifq-nw.a.run.app/";
 //export const ApiEndpoint = "http://127.0.0.1:5000/";
+export const ApiEndpoint = "https://basicsynbio-app.com/";
