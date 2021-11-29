6fd030be5a4e823f984a395348400e0bb540b684
Artem Sapegin <artem@sapegin.ru>
2021-11-29 19:19:59 +0000

    Functional JSON download

--- src/Components/designer/export/UniqueParts.Components/UniqueParts.tsx
+++ src/Components/designer/export/UniqueParts.Components/UniqueParts.tsx
@@ -39,7 +39,7 @@ export const UniqueParts: React.FC<Props> = ({ currentBuild }) => {
     console.log(response);
     console.log(response.data);
     var blob = new Blob([response.data], { type: "chemical/seq-na-genbank" });
-    saveAs(blob, "Unique_Assemblies.gb");
+    saveAs(blob, "Unique_Parts.gb");
     // fetch(ApiEndpoint + "builduniqueparts", {
     //   method: "POST",
     //   headers: {
